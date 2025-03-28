8a6025b6e66e0f5478e69548d2a235ef79ee5cdd
Ari Bisma <aribisma19@gmail.com>
2025-03-28 20:37:12 +0800

    second commit

--- student.php
+++ student.php
@@ -96,7 +96,7 @@
                 <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <a class="collapse-item" href="module_terbaru.php">Module Terbaru</a>
-                        <a class="collapse-item" href="module_terlaris.php">Module Terlaris</a>
+                        <a class="collapse-item" href="module_terlaris.php">Video Pelatihan</a>
                     </div>
                 </div>
             </li>
