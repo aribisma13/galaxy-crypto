c1dc9d106f3b16a4ad4a53d39b161088c5a35ab2
Ari Bisma <aribisma19@gmail.com>
2025-04-26 20:55:43 +0800

    three commit

--- module_student_akses.php
+++ module_student_akses.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Mentor | Galaxy Crypto</title>
+    <title>Module Pelatihan | Galaxy Crypto</title>
 
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/galaxy-crypto.ico" />
